12bec321a64de8924b61fd6e986f0d9e32ac4642
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2018-02-18 22:35:48 +0530

    escape/unescape templates while rendering

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function render(html, options) {
     var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
     var add = function(line, js) {
         js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+            (code += line != '' ? 'r.push("' + escape(line) + '");\n' : '');
         return add;
     }
     while(match = re.exec(html)) {
@@ -21,7 +21,7 @@ async function render(html, options) {
       }
     }
     const rendered = await evalInContext(code, options);
-    return rendered;
+    return unescape(rendered);
 }
 
 module.exports = render;
