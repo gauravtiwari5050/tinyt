c4841fd2af99c542c0e4ca595b486db2e48608ba
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2018-02-18 20:36:59 +0530

    initial import

---
+++ index.js
@@ -0,0 +1,27 @@
+async function render(html, options) {
+    var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0, match;
+    var add = function(line, js) {
+        js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+        return add;
+    }
+    while(match = re.exec(html)) {
+        add(html.slice(cursor, match.index))(match[1], true);
+        cursor = match.index + match[0].length;
+    }
+    add(html.substr(cursor, html.length - cursor));
+    code += 'return r.join("");';
+    code = `(async () => { ${code} })()`;
+
+    async function evalInContext(js, context) {
+      with(context) {
+        return async function() { 
+          return async function() { return await eval(js); }.apply(context);
+        }.call();
+      }
+    }
+    const rendered = await evalInContext(code, options);
+    return rendered;
+}
+
+module.exports.tinyt = render;
